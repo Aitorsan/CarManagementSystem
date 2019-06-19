ddfec9174593b05aec42a97c6a0eabc1cd4a4ce8
AitorSF <aitor.sanmartin11@gmail.com>
2019-06-19 15:42:54 +0200

    Update Typology.java

--- src/autoUdima/model/Typology.java
+++ src/autoUdima/model/Typology.java
@@ -1,24 +1,20 @@
 package autoUdima.model;
 
-/**
- * Enumeration represent different avaliable car types
- * @author aitorSf
- *
- */
-public enum Typology {
+public enum Typology 
+{
 	FAMILIAR("Familiar"),
 	DEPORTIVO("Deportivo"),
 	FURGONETA("Furgoneta");
-
 	private  String type;
 
-	Typology(String type) {
+	Typology(String type) 
+	{
 		this.type = type;
 	}
 
 	@Override
-	public String toString() {
-
+	public String toString() 
+	{
 		return type;
 	}
 	/**
@@ -26,20 +22,26 @@ public enum Typology {
 	 * @param type
 	 * @return
 	 */
-	public static Typology getTypeFromString(String type) {
+	public static Typology getTypeFromString(String type) 
+	{
 		Typology carType = null;
-		   
-		if( type != null) {
+		if( type != null) 
+		{
 			
-			if( type.toLowerCase().equals("familiar")) {
+			if( type.toLowerCase().equals("familiar")) 
+			{
 				carType= FAMILIAR;
-			}else if(type.toLowerCase().equals("deportivo")) {
+			}
+			else if(type.toLowerCase().equals("deportivo")) 
+			{
 				carType = DEPORTIVO;
-			}else if(type.toLowerCase().equals("furgoneta")) {
+			}
+			else if(type.toLowerCase().equals("furgoneta"))
+			{
 				carType = FURGONETA;
 			}
 		}
-			return carType;
+		return carType;
 	}
 
 }
