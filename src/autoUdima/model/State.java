3c7b2301d8044c2443c085efe7df97665f5151b2
AitorSF <aitor.sanmartin11@gmail.com>
2019-06-19 15:41:40 +0200

    Update State.java

--- src/autoUdima/model/State.java
+++ src/autoUdima/model/State.java
@@ -1,46 +1,38 @@
 package autoUdima.model;
 
-/**
- * Enumeracion que contiene los distintos estados que puede tener un coche.<br>
- * 
- * @author aitorSf
- *
- */
-public enum State {
+public enum State 
+{
        NUEVO("Nuevo"),
        SEMINUEVO("Seminuevo");
 	
 	private final String estado;
 	
-	State(String estado){
+	State(String estado)
+	{
 		this.estado = estado;
 	}
+	
 	@Override
-	public String toString() {
-		
+	public String toString() 
+	{
 		return estado;
 	}
 	
-	/**
-	 * Return a State enum type from a string
-	 * @param state
-	 * @return Estado
-	 */
-	public static State getStateFromString(String state) {
-
+	public static State getStateFromString(String state) 
+	{
 		State carState= null ;
-		if( state != null) {
+		if( state != null) 
+		{
 			state.toLowerCase();
-
-			if( state.toLowerCase().equals("nuevo")) {
+			if( state.toLowerCase().equals("nuevo")) 
+			{
 				carState = State.NUEVO;
-			}else if(state.toLowerCase().equals("seminuevo")) {
+			}
+			else if(state.toLowerCase().equals("seminuevo")) 
+			{
 				carState = State.SEMINUEVO;
 			}
-		
 		}
-
 		return carState;
-
 	}
 }
