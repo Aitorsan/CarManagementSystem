d27b024ccb23cb975d90ff2b324cb6814c1d2a28
AitorSF <aitor.sanmartin11@gmail.com>
2019-06-19 16:34:12 +0200

    Update Main.java

--- src/autoUdima/Main.java
+++ src/autoUdima/Main.java
@@ -1,12 +1,8 @@
 package autoUdima;
 
 import autoUdima.control.WindowHandlerControl;
-/**
- * All methods of the program are comented in blue, also the inner clases.<br>
- * The rest of the code its commented in green, algorithms, functions and some <br>
- * other relevant points of the system.
- * @author Aitor Sanmartin Ferreira (AA2) Metodologia de la programacion
- */
+
+ /** @author Aitor Sanmartin Ferreira (AA2) Metodologia de la programacion */
 public class Main 
 {
 	public static void main(String[] args) 
